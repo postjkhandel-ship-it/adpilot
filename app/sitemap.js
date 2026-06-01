998909a09259d106ad398c11ef22feb5bde081c8
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-06-01 17:01:40 +0200

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,22 +3,7 @@ export default function sitemap() {
     {
       url: "https://adpilot.dk",
       lastModified: new Date(),
-      priority: 1
+      priority: 1,
     },
-    {
-      url: "https://adpilot.dk/dashboard",
-      lastModified: new Date(),
-      priority: 0.8
-    },
-    {
-      url: "https://adpilot.dk/handelsbetingelser",
-      lastModified: new Date(),
-      priority: 0.5
-    },
-    {
-      url: "https://adpilot.dk/privatlivspolitik",
-      lastModified: new Date(),
-      priority: 0.5
-    }
   ];
 }
