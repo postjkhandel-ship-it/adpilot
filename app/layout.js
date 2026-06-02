778860853e4dcd6e60b8fc32c1a04752c6764560
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-06-02 15:58:05 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
-```js
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata = {
   title: "AdPilot – AI annoncegenerator til Meta Ads & Google Ads",
@@ -11,33 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="da">
-      <body>
-        {children}
-
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);
-            t.async=!0;
-            t.src=v;
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}
-            (window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-
-            fbq('init', '1525584212593660');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
-```
