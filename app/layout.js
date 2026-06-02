6632e5e52032b0c435a4977491ae2a15091ad7f2
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-06-02 16:17:56 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+```js
 import "./globals.css";
 
 export const metadata = {
@@ -9,7 +10,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="da">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}
+(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1525584212593660');
+fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
 }
+```
