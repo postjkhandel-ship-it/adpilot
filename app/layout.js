e3934c8d9880f3520aa162b26a8574ec1d749c2c
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-06-03 18:29:17 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="da">
+    <Script
+  src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+  strategy="afterInteractive"
+/>
       <body>
         {children}
         <MetaPixel />
