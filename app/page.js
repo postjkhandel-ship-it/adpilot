ceea0012826d2d61a6ca831213f492b570cb8aaf
postjkhandel-ship-it <postjkhandel@gmail.com>
2026-06-03 18:11:00 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -504,7 +504,9 @@ export default function Home() {
         <div>
           <h4>Kontakt</h4>
           <a href="mailto:kontakt@adpilot.dk">kontakt@adpilot.dk</a>
-          <span>CVR: 41329548</span>      
+          <span>CVR: 41329548</span>
+          <span>Kallerupvej 133</span>
+          <span>4400 Kalundborg</span>      
           <span>Danmark</span>
         </div>
       </footer>
